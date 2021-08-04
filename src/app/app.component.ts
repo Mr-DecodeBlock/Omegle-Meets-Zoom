748c9617b7bd7c0da7e4abc99588501ffde78565
asad bilal <asadbilal800@gmail.com>
2021-08-04 15:40:16 +0500

    imported socket io client lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {io, Socket} from "socket.io-client";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  socket;
+  serverMessage;
+  constructor() {
+  }
+
+  ngOnInit() {
+    console.log('connecting with server....')
+
+   // this.socket = io(' https://my-node-app-web-rtc.herokuapp.com')
+    this.socket = io(' http://localhost:3000')
+
+
+    this.socket.on('anyone', (msg)=> {
+
+      this.serverMessage = msg
+    })
+  }
+
   title = 'Omegle-Meets-Zoom';
 }
