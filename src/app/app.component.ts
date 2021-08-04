84383608cd13cba5e0a865980c541cd0dfddb0c0
asad bilal <asadbilal800@gmail.com>
2021-08-04 23:06:12 +0500

    socket io client infrastructure set and base logic.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import {Component, OnInit} from '@angular/core';
-import {io, Socket} from "socket.io-client";
+import {io} from "socket.io-client";
+
+
+export type clientMessageResponse = {
+  room : string,
+  message : string
+}
 
 @Component({
   selector: 'app-root',
@@ -9,20 +15,34 @@ import {io, Socket} from "socket.io-client";
 export class AppComponent implements OnInit{
   socket;
   serverMessage;
-  constructor() {
-  }
+  room;
+
 
   ngOnInit() {
     console.log('connecting with server....')
 
-   // this.socket = io(' https://my-node-app-web-rtc.herokuapp.com')
-    this.socket = io(' http://localhost:3000')
+  // this.socket = io('https://my-node-app-web-rtc.herokuapp.com')
+    this.socket = io('http://localhost:3000')
 
+    this.socket.on('room',(room)=> {
+      //if room is not set
+      if(!!room) {
+        this.room = room
+        console.log('entered a room')
+        console.log(room)
+        let messageModel: clientMessageResponse ={
+          room : room,
+          message : 'Hello from the other side'
+        }
+        this.socket.emit('send-message', messageModel)
+      }
 
-    this.socket.on('anyone', (msg)=> {
-
-      this.serverMessage = msg
     })
+
+    this.socket.on('send-message',(msg)=> {
+      console.log(msg)
+    })
+
   }
 
   title = 'Omegle-Meets-Zoom';
